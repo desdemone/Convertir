8d2ee68ff727128e3afae2b5893e2ff030b2fe4c
desdemone <v.nourelsantoro@hotmail.fr>
2022-02-13 16:08:50 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Convertisseur/>
+</template>
+
+<script>
+import Convertisseur from'./components/Convertisseur.vue'
+export default {
+  name: 'App',
+  components: {
+   Convertisseur
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  background: no-repeat url('./assets/photo.jpg');
+  background-size: cover;
+}
+</style>
---
+++ src/components/Convertisseur.vue
@@ -0,0 +1,76 @@
+<template>
+  <div :style="{'background-image':'url(./assets/photo.jpg)'}">
+  </div>
+<h1>Conversion g/l => mmol/l</h1>
+
+    <div class="bloc">
+
+      <select v-model="choix">
+        <option value="grammes">Grammes</option>
+        <option value="mmol">Mmol</option>
+      </select>
+      <button v-on:click="calcul">Conversion</button>
+    </div>
+
+    <div class="bloc1">
+      <div class="input">
+        <input placeholder="saisissez la glycémie" v-model="glycemie" >
+      </div>
+    </div>
+    <p class="result"> <strong> Résultat :</strong> {{result}} </p>
+
+
+
+</template>
+
+<script>
+export default{
+  name:"Convertisseur",
+  data() {
+        return {
+          value: '',
+          glycemie: '',
+          choix: '',
+          result: 0,
+
+        }
+      },
+
+      methods: {
+        calcul() {
+          if (this.choix === 'grammes') {
+            this.result = this.glycemie * 5.5 + " mmol";
+          } else if (this.choix === 'mmol') {
+            this.result = this.glycemie * 0.18 + " grammes"
+          }
+          return this.result.toFixed(2)
+        }
+      }
+
+}
+</script>
+<style scoped>
+
+  .bloc {
+    margin-left: 400px;
+
+  }
+
+  button {
+    margin-left: 20px;
+    margin-top: 100px;
+  }
+
+  .bloc1 {
+    margin-top: -20px;
+
+  }
+
+
+  h1 {
+    margin-bottom: 100px;
+    text-align: center;
+    font-style: italic;
+    font-size: 60px;
+  }
+</style>
